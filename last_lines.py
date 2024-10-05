1c7aa4960943a76c987e1986f70693c1c23ceeb5
Rudá Floresta <floresta.ruda@gmail.com>
2024-10-05 19:39:58 -0300

    adds read_size parameter

--- last_lines.py
+++ last_lines.py
@@ -1,18 +1,21 @@
 import io
 import os
 
-def last_lines(filename):
-    read_size = io.DEFAULT_BUFFER_SIZE
+def last_lines(filename, read_size=io.DEFAULT_BUFFER_SIZE):
     file_iterator = []
     with open(filename, 'r') as file:
         file_size = os.fstat(file.fileno()).st_size 
-        seek_position = max(file_size - read_size, 0)
-
+        seek_position = file_size
         while seek_position >= 0:
             file.seek(seek_position)
-            file_iterator.append(repr(file.read(read_size)))
+            try:
+                current_line = repr(file.readline(read_size))
+                file_iterator.append(current_line)
+            except Exception:
+                seek_position -= 1
             seek_position -= read_size
     return iter(file_iterator)
 
+lines = last_lines('text_file.txt', 3)
 
-lines = last_lines('text_file.txt') 
+
